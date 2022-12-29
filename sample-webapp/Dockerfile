dc6274436718a3bcdcde882a0ae90b8cc8381d71
HaraT0323 <alra9si916323@gmail.com>
2022-12-29 22:47:58 +0900

    sample-webappのアプリ、Docker作成

---
+++ sample-webapp/Dockerfile
@@ -0,0 +1,10 @@
+FROM node:16
+
+RUN npm install -g http-server
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+EXPOSE 8080
+CMD ["http-server", "dist"]
---
+++ sample-webapp/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>This application is Sample WebApp for CICD lesson.</h1>
+    <div>Response from REST API is... {{ info }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      info: null
+    }
+  }
+  ,
+  mounted() {
+    const BACKEND_URL = 'https://ecs-test.tk/api'
+    axios
+      .get(BACKEND_URL)
+      .then(response => (this.info = response.data))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
